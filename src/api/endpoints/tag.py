e39cd374abd3be7f94e9d5b06873c7860e8736d9
ruben <ruben.painenao20@gmail.com>
2025-12-23 11:47:34 -0300

    Feat: update get_tags function to use DBSessionDep for database dependency

--- src/api/endpoints/tag.py
+++ src/api/endpoints/tag.py
@@ -4,13 +4,14 @@ from typing import List
 from src.api.deps import get_db 
 from src.crud import tag as crud
 from src.schemas import tag as schemas
+from src.db.transactional import DBSessionDep
 
 
 router = APIRouter()
 
 
 @router.get("", response_model=List[schemas.Tag])
-def get_tags(db: Session = Depends(get_db)):
+def get_tags(db: DBSessionDep):
     """Get all tags."""
     tags = crud.get_all_tags(db)
     return tags
